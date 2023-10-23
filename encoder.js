f31dbf17644fd65f50016d7bf2eb461603daa743
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-10-23 09:40:35 +0200

    Encoder

--- encoder.js
+++ encoder.js
@@ -1,10 +1,26 @@
-function solution(roman) {
-    let ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
-        tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
-        hundreds = [ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
-        thousands = ["", "M", "MM", "MMM"];
-    return thousands[Math.floor(roman / 1000)] +
-           hundreds[Math.floor((roman % 1000) / 100)] +
-           tens[Math.floor((roman % 100) / 10)] +
-           ones[roman % 10];
+const romanNumerals = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I"
 }
+
+function convertToRoman(roman) {
+    return romanNumerals[1000][Math.floor(roman / 1000)] +
+           romanNumerals[900][(Math.floor(roman % 1000 / 100) * 100)] +
+           romanNumerals[100][Math.floor((roman % 100) / 10) * 10] +
+           romanNumerals[10][roman % 10];
+}
+
+
+console.log(convertToRoman(14));
+
